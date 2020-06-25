1231c4a41f17328c7cebd0971463df1d6449d7a3
ptccong <congpt.bhsoft@gmail.com>
2020-06-25 15:57:40 +0700

    Initial commit

---
+++ StackOverFlow/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StackOverFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post p = new Post("Sử lý lỗi xamarin ","ạdklasdhasjdhaskjhdjkashdjkashdjkashdjkah",2);
+            p.voteUp();
+            p.voteDow();
+            p.showPost();
+        }
+    }
+    class Post
+    {
+        private string title { set; get; }
+        private string detail { set; get; }
+        private DateTime datepost = DateTime.Now;
+        private int countVotes = 0;
+        public Post() { }
+        public Post(string title,string detail,int countVotes)
+        {
+            this.title = title;
+            this.detail = detail;
+            this.countVotes = countVotes;
+        }
+        public void voteUp()
+        {
+            countVotes+=5;
+        }
+        public void voteDow()
+        {
+            countVotes--;
+        }
+        public void showVote()
+        {
+            
+        }
+        public void showPost()
+        {
+            Console.WriteLine("title: "+title+" \ndetail: "+ detail+"\nDatepost: "+datepost+"\nVote: "+countVotes);
+        }
+    }
+}
